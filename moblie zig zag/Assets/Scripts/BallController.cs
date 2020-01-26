88822c4d3caf28d680ba31b73d16136f39bc62bc
AidenBow <AidenBow@users.noreply.github.com>
2020-01-26 15:35:33 -0800

    ball controller moves on click

--- moblie zig zag/Assets/Scripts/BallController.cs
+++ moblie zig zag/Assets/Scripts/BallController.cs
@@ -6,15 +6,43 @@ public class BallController : MonoBehaviour
 {
     [SerializeField]
     private float speed;
-    // Start is called before the first frame update
+    bool started;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent< Rigidbody > ();
+    }
     void Start()
     {
-        
+        rb.velocity = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDirection();
+        }
+    }
+
+    void SwitchDirection()
+    {
+        if(rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        } else
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
     }
 }
