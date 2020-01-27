b5f540e92d4867628a2bbdbfe39bf8381c4785f5
AidenBow <AidenBow@users.noreply.github.com>
2020-01-26 18:34:04 -0800

    fall down script

---
+++ moblie zig zag/Assets/Scripts/fallTriggerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallTriggerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+      if (col.gameObject.tag == "Ball")
+        {
+            Invoke("FallDown", 0.2f);
+        }
+    }
+
+    void FallDown()
+    {
+        GetComponentInParent<Rigidbody>().useGravity = true;
+        Destroy(transform.parent.gameObject, 2f);
+    }
+}
